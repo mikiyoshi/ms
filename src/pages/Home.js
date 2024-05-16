41cef3d67893e3d28667c01990f776000faf9e4a
mikiyoshi <mikiyoshi.kokura@gmail.com>
2024-05-16 19:10:15 -0400

    update category2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,26 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 
 export default function Home({ post }) {
 	const [featuredImage, setFeaturedimage] = useState();
 
-	const getImage = () => {
-		axios.get(post?._links["wp:featuredmedia"][0]?.href).then((response) => {
-			setFeaturedimage(response.data.source_url);
-		});
-	};
+	const getImage = useCallback(() => {
+		axios
+			.get(post?._links["wp:featuredmedia"][0]?.href)
+			.then((response) => {
+				setFeaturedimage(response.data.source_url);
+			})
+			.catch((error) => console.log(error));
+	}, [post]); // post が変更された場合にのみ getImage を再生成
 
 	useEffect(() => {
 		getImage();
-	}, []);
+	}, [getImage]);
 
 	return (
 		<div class="container">
 			<h1>Home</h1>
+			<img src={featuredImage} alt={post.title.rendered} class="mask" />
 		</div>
 	);
 }
--- src/pages/Service.js
+++ src/pages/Service.js
@@ -1,23 +1,27 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 
 export default function Service({ post }) {
 	const [featuredImage, setFeaturedimage] = useState();
 
-	const getImage = () => {
-		axios.get(post?._links["wp:featuredmedia"][0]?.href).then((response) => {
-			setFeaturedimage(response.data.source_url);
-		});
-	};
+	const getImage = useCallback(() => {
+		axios
+			.get(post?._links["wp:featuredmedia"][0]?.href)
+			.then((response) => {
+				setFeaturedimage(response.data.source_url);
+			})
+			.catch((error) => console.log(error));
+	}, [post]); // post が変更された場合にのみ getImage を再生成
 
 	useEffect(() => {
 		getImage();
-	}, []);
+	}, [getImage]);
 
 	return (
 		<div class="container">
 			<h1>Service</h1>
 			<div>{post.id}</div>
+			<img src={featuredImage} alt={post.title.rendered} class="mask" />
 		</div>
 	);
 }
