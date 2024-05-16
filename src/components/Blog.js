46c567c244997c9877cb70bdc1c12e1a90c0782f
mikiyoshi <mikiyoshi.kokura@gmail.com>
2024-05-16 18:44:46 -0400

    update Blog5 chat gpt help

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import "./blog.css";
 
 export default function Blog({ post }) {
