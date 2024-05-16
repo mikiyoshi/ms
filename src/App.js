049259e57b1cbd000f0d1cb4561799316314c7a1
mikiyoshi <mikiyoshi.kokura@gmail.com>
2024-05-16 18:55:55 -0400

    update category

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 import Blog from "./components/Blog";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import Service from "./pages/Service";
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import Careers from "./pages/Careers";
 
 const App = () => {
 	const [posts, setPosts] = useState([]);
@@ -21,6 +27,24 @@ const App = () => {
 	return (
 		<>
 			<p className="text-7xl text-blue-700">Hello</p>
+			{posts.map((item, index) => (
+				<Home key={index} post={item} />
+			))}
+			{posts.map((item, index) => (
+				<Services key={index} post={item} />
+			))}
+			{posts.map((item, index) => (
+				<Service key={index} post={item} />
+			))}
+			{posts.map((item, index) => (
+				<AboutUs key={index} post={item} />
+			))}
+			{posts.map((item, index) => (
+				<ContactUs key={index} post={item} />
+			))}
+			{posts.map((item, index) => (
+				<Careers key={index} post={item} />
+			))}
 			{posts.map((item, index) => (
 				<Blog key={index} post={item} />
 			))}
