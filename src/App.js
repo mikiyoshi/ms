41a16440b962754a2fe4c5a8a1d9930a330679f3
mikiyoshi <mikiyoshi.kokura@gmail.com>
2024-05-16 18:00:11 -0400

    update backup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "./App.css";
 
-export default function App() {
-	const [posts, setPosts] = useState([]);
-
-	const fetchPosts = () => {
+const App = () => {
+	const [data, setData] = useState([]);
+	useEffect(() => {
 		axios
 			.get("//mindfulseeds.torontosharehouse.com/wp/wp-json/wp/v2/posts")
-			.then((res) => {
-				setPosts(res.data);
-			});
-	};
-
-	useEffect(() => {
-		fetchPosts();
+			.then((response) => setData(response.data))
+			.catch((error) => console.log(error));
 	}, []);
-
 	return (
-		<div>
-			<p className="text-7xl text-blue-700">Hello</p>
-		</div>
+		<>
+			{data.map((item, index) => (
+				<li key={index}>
+					<a href={item.link}>
+						<date>{item.date}</date>
+						<p>{item.guid.rendered}</p>
+						<p>{item.slug}</p>
+						<p>{item.link}</p>
+						<p>{item.title.rendered}</p>
+						<p>{item.content.rendered}</p>
+						<div dangerouslySetInnerHTML={{ __html: item.content.rendered }} />
+						<p>{item.excerpt.rendered}</p>
+						<p>{item.author}</p>
+						<p>{item.featured_media}</p>
+					</a>
+				</li>
+			))}
+		</>
 	);
-}
+};
+
+export default App;
