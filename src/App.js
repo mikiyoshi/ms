41cc150e7827c49e567d407059effa3489e7c176
mikiyoshi <mikiyoshi.kokura@gmail.com>
2024-05-19 11:53:44 -0400

    update react router dom2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import axios from "axios";
 import Blog from "./components/Blog";
 import Home from "./pages/Home";
